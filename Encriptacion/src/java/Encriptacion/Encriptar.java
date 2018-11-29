fa8440a7ea8e80e964f856012e4cf8c08c037820
joelcuadros <jcuadros@vallegrande.edu.pe>
2018-11-29 11:10:36 -0500

    --

--- Encriptacion/src/java/Encriptacion/Encriptar.java
+++ Encriptacion/src/java/Encriptacion/Encriptar.java
@@ -1,36 +1,32 @@
 package Encriptacion;
 
+import java.security.MessageDigest;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 public class Encriptar {
 
-    public static String getHash(String txt, String hashType) {
+    public static String getHash(String texto) {
         try {
-            java.security.MessageDigest md = java.security.MessageDigest
-                    .getInstance(hashType);
-            byte[] array = md.digest(txt.getBytes());
-            StringBuffer sb = new StringBuffer();
-            for (int i = 0; i < array.length; ++i) {
-                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100)
-                        .substring(1, 3));
+            MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+            String passWithSalt = texto;
+            byte[] passBytes = passWithSalt.getBytes();
+            byte[] passHash = sha256.digest(passBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < passHash.length; i++) {
+                sb.append(Integer.toString((passHash[i] & 0xff) + 0x100, 16).substring(1));
             }
-            return sb.toString();
+            String generatedPassword = sb.toString();
+            return generatedPassword;
         } catch (java.security.NoSuchAlgorithmException e) {
             System.out.println(e.getMessage());
         }
         return null;
     }
 
-    /* Retorna un hash SHA1 a partir de un texto */
-    public static String sha1(String txt) {
-        return Encriptar.getHash(txt, "SHA1");
-    }
-
-    public static void main(String[] args) {
-//        System.out.println(Encriptar.sha1("hola mundo"));
-        String a = Encriptar.cifradoCesar("hola mundo", 123456789);
-        System.out.println(Encriptar.sha1(a));
-    }
-
-    public static String cifradoCesar(String texto, double codigo) {
+    public static String cifradoCesar(String texto, int codigo) {
         StringBuilder cifrado = new StringBuilder();
         codigo = codigo % 26;
         for (int i = 0; i < texto.length(); i++) {
@@ -49,5 +45,72 @@ public class Encriptar {
             }
         }
         return cifrado.toString();
+    }
+
+    public static String descifradoCesar(String texto, int codigo) {
+        StringBuilder cifrado = new StringBuilder();
+        codigo = codigo % 26;
+        for (int i = 0; i < texto.length(); i++) {
+            if (texto.charAt(i) >= 'a' && texto.charAt(i) <= 'z') {
+                if ((texto.charAt(i) - codigo) < 'a') {
+                    cifrado.append((char) (texto.charAt(i) - codigo + 26));
+                } else {
+                    cifrado.append((char) (texto.charAt(i) - codigo));
+                }
+            } else if (texto.charAt(i) >= 'A' && texto.charAt(i) <= 'Z') {
+                if ((texto.charAt(i) - codigo) < 'A') {
+                    cifrado.append((char) (texto.charAt(i) - codigo + 26));
+                } else {
+                    cifrado.append((char) (texto.charAt(i) - codigo));
+                }
+            }
+        }
+        return cifrado.toString();
+    }
+
+    public static String AES(String key, String initVector, String value) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            String s = new String(Base64.getEncoder().encode(encrypted));
+            return s;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String desencriptar(String key, String initVector, String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            byte[] original = cipher.doFinal(Base64.getDecoder().decode(encrypted));
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String a = Encriptar.cifradoCesar("hola mundo", 123456789);
+        System.out.println("Cesar -->" + a);
+        String initVector = "RandomInitVector";
+        String b = AES("Bar12345Bar12345", initVector, a);
+        System.out.println("AES -->" + b);
+        System.out.println("SHA2-->" + Encriptar.getHash(b));
+//        System.out.println("AES Desencriptar-->" + Encriptar.desencriptar("Bar12345Bar12345", initVector, b));
+//        System.out.println("Cesar Desencriptar-->" + descifradoCesar(Encriptar.desencriptar("Bar12345Bar12345", initVector, b), 123456789));
     }
 }
